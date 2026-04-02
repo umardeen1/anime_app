630968e9916557265fb2e7133f191f06a9bb646b
Umar Deen <105164056+umardeen1@users.noreply.github.com>
2026-04-02 10:18:49 +0530

    error

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ import 'package:anime_app/pages/search.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'services/api_service.dart';
-import 'bloc/anime_bloc.dart';
+import 'bloc/anime_bloc.dart'
 # i love flutter and don't want to continue 
 void main() {
   runApp(const AnimeHub());
